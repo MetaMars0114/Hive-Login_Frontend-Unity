c87ab0593521ab6307ef313b908280ad8beb8969
MetaMars0114 <alinazvirdew@gmail.com>
2022-09-22 04:59:48 +0900

    verify to sign

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { login, login_keychain } from "../actions/actions"
 import { connect } from "react-redux";
-import backend from "../api/backend";
 const dhive = require('@hivechain/dhive');
-
+const encryptionHelper = require("./encryptionhelper");
+const sanitize = require("xss");
+const client = new dhive.Client('https://anyx.io');
+const steem = require('steem');
+require('dotenv').config();
+const algorithm = "aes-128-cbc";
 class LoginParking extends React.Component {
 
     state = { username: "", error: "", loginType: "", showBnt: true };
@@ -32,20 +36,21 @@ class LoginParking extends React.Component {
     send_login_token = async () => {
 
         let keychain = window.hive_keychain;
-        let memo = (await backend.post('/auth/keychain/fetch_memo', { username: this.state.username })).data;
-
-        console.log('memo', memo);
-
-        if (memo.status === "ok") {
-            keychain.requestVerifyKey(this.state.username, memo.message, "Posting", (response) => {
+        // let memo = (await backend.post('/auth/keychain/fetch_memo', { username: this.state.username })).data;
+        let memo_fetching= await getting_fetch_memo(this.state.username);
+        // console.log('memo', memo);
+        console.log("memo_fetching in send_login_token", memo_fetching);
+        if (memo_fetching.status === "ok") {
+            // keychain.requestVerifyKey(this.state.username, memo_fetching.message, "Posting", (response) => {
+                keychain.requestSignBuffer(this.state.username, memo_fetching.message, "Posting", (response) => {
                 if (response.success === true) {
                     // this.props.login_keychain(this.state.username, response.result);
                     console.log('log in successed::: ', this.state.username);
                     window.dragon.walletConnect(this.state.username);
                 }
-            });
+            },null,"SIGN WOO");
         } else {
-            this.setState({ error: "There was an error with the backend server, please try again" });
+            this.setState({ error: "There was an error while fetching_memo" });
         }
 
     };
@@ -62,31 +67,34 @@ class LoginParking extends React.Component {
             });
 
             let data = await this.client.database.getAccounts([this.state.username]);
-            console.log('data in login_keychain', data);
             if (data.length === 1) {
 
-                let auth = data[0].posting.account_auths.filter(el => el[0] === "downvote-tool");
-                console.log('auth in login_keychain', auth);
+                // let auth = data[0].posting.account_auths.filter(el => el[0] === "downvote-tool");
+                // console.log('auth in login_keychain', auth);
 
-                if (auth.length === 0) {
+                // if (auth.length === 0) {
 
-                    keychain.requestAddAccountAuthority(this.state.username, "downvote-tool", "posting", 1, (response) => {
-                        console.log('requestAddAccountAuthority in login_keychain', response);
+                //     keychain.requestAddAccountAuthority(this.state.username, "downvote-tool", "posting", 1, (response) => {
+                //         console.log('requestAddAccountAuthority in login_keychain', response);
 
-                        if (response.success === true)
-                            this.send_login_token();
-                        else
-                            this.setState({ error: "Keychain error" });
-                    });
-                } else {
+                //         if (response.success === true)
+                //             this.send_login_token();
+                //         else
+                //             this.setState({ error: "Keychain error" });
+                //     });
+                // } else {
                     this.send_login_token();
-                }
+                // }
             } else {
                 this.setState({ error: "Hive user not found" });
             }
         } else {
             this.setState({ error: "You do not have hive keychain installed" });
         }
+    };
+    login_with_username = async (event) => { 
+        console.log("in login_with_username", this.state.username);
+        window.dragon.walletConnect(this.state.username);
     };
 
     render() {
@@ -114,9 +122,20 @@ class LoginParking extends React.Component {
                                     marginTop: "20px",
                                     border: "1px solid #999999",
                                     borderRadius: "0"
-                                }}>Log in with keychain
+                                }}>Sign in with keychain
                                 </button>
+                                <button type={"button"} className="btn btn-primary " onClick={this.login_with_username} style={{
+                                    backgroundColor: "white",
+                                    color: "#999999",
+                                    width: "235px",
+                                    marginTop: "20px",
+                                    border: "1px solid #999999",
+                                    borderRadius: "0"
+                                }}>Log in with UserName
+                                </button>
+                                
                             </form>
+                            
                         </div>
                     </div>
                 }
@@ -124,6 +143,85 @@ class LoginParking extends React.Component {
         )
     }
 
+}
+
+async function getting_fetch_memo(usernameHive)  {
+    const hive_username = sanitize(usernameHive);
+
+    if (hive_username && hive_username.length < 16 && hive_username.length > 3) {
+        let data = await client.database.getAccounts([hive_username]);
+
+        console.log("data in getting fetch memo", data);
+        
+        let pub_key = data[0].posting.key_auths[0][0];
+
+        if (data.length === 1) {
+            let encoded_message = "";
+                
+            let { encrypted, iv } = await encrypt(hive_username);
+            encrypted = "#" + encrypted;
+            let wif = "5JRMuVZhkeE32Wfu1r62hr7jGdbmsXMPkgtC2Uid3pvhk9YSaQr";//have to insert in env
+            encoded_message = steem.memo.encode(wif, pub_key, encrypted);
+            console.log("encoded_message", encoded_message);
+            return ({ status: "ok", message: encoded_message });
+           
+
+        } else {
+            return({ status: "ko" });
+        }
+    }
+};
+
+function encrypt(text, initialisation_vector) {
+    return new Promise(async resolve => {
+        console.log("in resolve");
+        let data;
+        let iv;
+        console.log("initialisation_vector",initialisation_vector)
+        if (initialisation_vector) {
+            data = await get_encryption_data(initialisation_vector);
+            iv = initialisation_vector;
+            console.log("data", data);
+        }
+        else {
+            data = await get_encryption_data();
+            console.log("data", data);
+            let json_iv = JSON.stringify(data['iv']);
+            json_iv = JSON.parse(json_iv);
+
+            if (!json_iv.data)
+                console.log(json_iv);
+
+            iv = json_iv.data.toString();
+        }
+
+        // console.log("in encrypt:", algorithm);
+
+        //const encText = encryptionHelper.encryptText(data.key, data.iv);
+        console.log('before encrypt:', text, data.iv.toString())
+        const encText = encryptionHelper.encryptText(algorithm, data.key, data.iv, text, "hex");
+        let encrypted = "#" + encText;
+        console.log('after encrypt:', encrypted)
+
+        return resolve({ encrypted: encText, iv: iv });
+    });
+}
+
+function get_encryption_data(iv) {
+    console.log("get_encryption_data", get_encryption_data);
+    return new Promise(async resolve => {
+        let encryption_pw = "1234567890123456";
+        let encryption_data = await encryptionHelper.getKeyAndIV(encryption_pw);//(process.env.ENCRYPTION_PW);//have to insert in env
+        if (iv) {
+            iv = iv.split(",").map(Number);
+            const buf = Buffer.from(iv);
+            const vue = new Uint8Array(buf);
+            encryption_data['iv'] = vue;
+            return resolve(encryption_data);
+        }
+        else
+            return resolve(encryption_data);
+    });
 }
 
 
---
+++ src/components/encryptionhelper.js
@@ -0,0 +1,60 @@
+"use strict";
+require('dotenv').config()
+var crypto = require("crypto");
+var CryptoJS = require("crypto-js");
+
+var EncryptionHelper = (function () {
+
+    function getKeyAndIV(key) {
+
+        return new Promise(resolve => {
+            crypto.pseudoRandomBytes(16, function (err, ivBuffer) {
+               
+                var keyBuffer = (key instanceof Buffer) ? key : Buffer.from(key);
+                console.log("in getKeyAndIV", keyBuffer);
+
+                return resolve({
+                    iv: ivBuffer,
+                    key: keyBuffer
+                });
+            });
+        });
+    }
+
+    function encryptText(cipher_alg, key, iv, text, encoding) {
+        // var cipher = crypto.createCipheriv(cipher_alg, key, iv);
+
+        // encoding = encoding || "binary";
+
+        // var result = cipher.update(text, "utf8", encoding);
+        // result += cipher.final(encoding);
+
+        // return result;
+
+
+        var encrypted = CryptoJS.AES.encrypt(text, iv.toString());
+
+        return encrypted;
+
+    }
+
+    function decryptText(cipher_alg, key, iv, text, encoding) {
+
+        var decrypted = CryptoJS.AES.decrypt(text, iv.toString());
+        return decrypted;
+    }
+
+    return {
+        CIPHERS: {
+            "AES_128": "aes128",          //requires 16 byte key
+            "AES_128_CBC": "aes-128-cbc", //requires 16 byte key
+            "AES_192": "aes192",          //requires 24 byte key
+            "AES_256": "aes256"           //requires 32 byte key
+        },
+        getKeyAndIV: getKeyAndIV,
+        encryptText: encryptText,
+        decryptText: decryptText
+    };
+})();
+
+module.exports = EncryptionHelper;
